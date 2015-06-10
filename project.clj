840ada866b74cd9f5ddb51d3f81a7a65c8d1d548
Shea Levy <shea@shealevy.com>
2015-06-10 09:47:48 -0400

    Convert project dependency list to aether Dependency list

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [leiningen-core "2.5.1"]]
+                 [leiningen-core "2.5.1"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.eclipse.aether/aether-api "1.0.2.v20150114"]]
   :main ^:skip-aot lein2nix.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/lein2nix/core.clj
+++ src/lein2nix/core.clj
@@ -1,7 +1,59 @@
 (ns lein2nix.core
+  (:require [leiningen.core.project :as project]
+            [clojure.tools.logging :as log])
+  (:import (org.eclipse.aether.graph Dependency Exclusion)
+           (org.eclipse.aether.artifact DefaultArtifact))
   (:gen-class))
 
+(defn lein-exclusion-to-aether-exclusion
+  "Convert a lein exclusion vector to an aether Exclusion"
+  [excl]
+  (let [excl0 (nth excl 0)
+        exclMap (reduce (fn [acc kv]
+                          (case (first kv)
+                            :classifier (assoc acc :classifier (fnext kv))
+                            :extension (assoc acc :extension (fnext kv))))
+                        {:groupId (or (namespace excl0) (name excl0)),
+                         :artifactId (name excl0)} (partition 2
+                                                              (subvec excl 1)))]
+    (Exclusion. (exclMap :groupId) (exclMap :artifactId) (exclMap :classifier)
+                (exclMap :extension))))
+(defn lein-dependency-to-aether-dependency
+  "Convert a lein dependency vector to an aether Dependency"
+  [dep]
+  (let [dep0 (nth dep 0)
+        depMap (reduce (fn [acc kv]
+                         (case (first kv)
+                           :extension (assoc acc :extension (fnext kv))
+                           :scope (assoc acc :scope (fnext kv))
+                           :optional (assoc acc :scope (fnext kv))
+                           :exclusions
+                           (assoc acc :exclusions
+                                  (map lein-exclusion-to-aether-exclusion
+                                       (fnext kv)))
+                           :native-prefix acc
+                           (do
+                             (log/warn "Unknown key in dependency specification")
+                             (assoc acc :properties
+                                    (assoc
+                                      (acc :properties)
+                                      (first kv)
+                                      (fnext kv))))))
+                         {:groupId (or (namespace dep0) (name dep0)),
+                          :artifactId (name dep0),
+                          :version (nth dep 1),
+                          :extension "jar",
+                          :scope "compile",
+                          :properties {},
+                          :optional false} (partition 2 (subvec dep 2)))]
+    (Dependency. (DefaultArtifact. (depMap :groupId) (depMap :artifactId)
+                                 (depMap :classifier) (depMap :extension)
+                                 (depMap :version) (depMap :properties) nil)
+                (depMap :scope) (depMap :optional) (depMap :exclusions))))
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  ([arg]
+    (map
+      lein-dependency-to-aether-dependency
+      ((project/read arg) :dependencies)))
+  ([] (-main "project.clj")))
